d5b785213ba2e6457757eeb609ac8c73ddc71e54
Muhammad Saqlain Amin <muhammadsaqi476@gmail.com>
2026-05-07 21:35:16 +0500

    update time to 22

--- dags/Weather_ETL.py
+++ dags/Weather_ETL.py
@@ -1,13 +1,13 @@
-from airflow.sdk import dag, task
+from airflow.sdk import dag, task, Variable
 from pendulum import datetime
 from airflow.timetables.interval import CronDataIntervalTimetable
-from airflow.models import Variable
 from airflow.providers.mysql.hooks.mysql import MySqlHook
 import requests
+from datetime import datetime as dt
 
 @dag(
     dag_id="Weather_ETL",
-    schedule=CronDataIntervalTimetable("0 18 * * *", timezone="Asia/Karachi"),
+    schedule=CronDataIntervalTimetable("0 22 * * *", timezone="Asia/Karachi"),
     start_date=datetime(2026, 5, 5),
     end_date=datetime(2026, 6, 30),
     catchup=True,
@@ -16,62 +16,56 @@ def Weather_ETL():
 
     @task.python
     def extract_data_api():
-        api_key = Variable.get("OPENWEATHERMAP_API_KEY")
+        api_key = Variable.get("OPENWEATHER_API_KEY")
         city_name = "Lahore"
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric"
-
-        try:
-
-            response = requests.get(url)
-            if response.status_code == 200:
-                return response.json()
-            else:
-                raise Exception(f"API failed: {response.status_code}")
-        except Exception as e:
-            print(f"Error fetching data: {e}")
-            return None    
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        return response.json()
 
     @task.python
-    def transform_data(data, **kwargs):
+    def transform_data(data, logical_date=None):
         if data is None:
             raise ValueError("No data received!")
-        
-        execution_date = kwargs['ds']
-        
-        transformed= {
+
+        execution_date = str(logical_date.date()) if logical_date else ''
+
+        return {
             'city': data['name'],
             'min_temp': data['main']['temp_min'],
             'max_temp': data['main']['temp_max'],
             'humidity': data['main']['humidity'],
+            'recorded_at': dt.utcnow().strftime('%Y-%m-%d %H:%M:%S'),
             'execution_date': execution_date
         }
-        return transformed  
 
     @task.python
     def load_data(transformed):
         if transformed is None:
             raise ValueError("No data to load!")
-        
+
         mysql_hook = MySqlHook(mysql_conn_id='mysql_local')
-        
+
         sql = """
-        INSERT INTO weather_forecast (city, min_temp, max_temp, humidity, execution_date)
-        VALUES (%s, %s, %s, %s, %s)
-        ON DUPLICATE KEY UPDATE 
+        INSERT INTO   etl_weather_pipline_data.weather_forecast (city, min_temp, max_temp, humidity, recorded_at, execution_date)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        ON DUPLICATE KEY UPDATE
             min_temp = VALUES(min_temp),
             max_temp = VALUES(max_temp),
-            humidity = VALUES(humidity);
+            humidity = VALUES(humidity),
+            recorded_at = VALUES(recorded_at);
         """
-        
+
         mysql_hook.run(sql, parameters=(
             transformed['city'],
             transformed['min_temp'],
             transformed['max_temp'],
             transformed['humidity'],
+            transformed['recorded_at'],
             transformed['execution_date']
         ))
-        
-        print(f" Data loaded for {transformed['city']}!")
+
+        print(f"Data loaded for {transformed['city']} at {transformed['recorded_at']}!")
 
     extracted = extract_data_api()
     transformed = transform_data(extracted)
